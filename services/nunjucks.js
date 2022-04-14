0177983e2742dcd6aa8347545068c23db2ed56f9
KiiDii <kiidii.aniname@gmail.com>
2022-04-14 17:39:55 +0300

    🐛 FIX: bug with parsing \n and \t symbols

--- services/nunjucks.js
+++ services/nunjucks.js
@@ -8,9 +8,19 @@ const replace  = require('lodash/replace');
  * @returns {object} Parsed object
  */
 function JSONparseTemplateString(string) {
-  return JSON.parse(string, (key, value) => {
+  let preparedString = string;
+
+  preparedString = replace(preparedString, /\n/g, '\\n');
+
+  return JSON.parse(preparedString, (key, value) => {
     try {
-      return JSON.parse(replace(value, /&quot;/g, '"'));
+      let preparedValue = value;
+
+      preparedValue = replace(preparedValue, /\n/g, '\\n');
+      preparedValue = replace(preparedValue, /\t/g, '\\t');
+      preparedValue = replace(preparedValue, /&quot;/g, '"');
+
+      return JSON.parse(preparedValue);
     }
 
     catch {
